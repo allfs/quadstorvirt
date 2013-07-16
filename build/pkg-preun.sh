ce7f13a4b53985a551a1edaeee7106b918939db8
Shivaram U <shivaram.u@quadstor.com>
2013-07-16 11:06:00 +0530

    Cleanup html and cgiscripts during uninstall

--- build/pkg-preun.sh
+++ build/pkg-preun.sh
@@ -1,8 +1,33 @@
 #!/bin/sh
+
+if [ -d /usr/local/www/apache22/ ]; then
+	htdocs=/usr/local/www/apache22/data;
+	cgibin=/usr/local/www/apache22/cgi-bin;
+elif [ -d /usr/local/www/ ]; then
+	htdocs=/usr/local/www/data;
+	cgibin=/usr/local/www/cgi-bin;
+else
+	htdocs=/var/www/html
+	cgibin=/var/www/cgi-bin
+fi
+
+cgilist=`cd /quadstor/httpd/cgi-bin && ls -1 *.cgi`
+for i in $cgilist; do
+	rm -f $cgibin/$i
+done
+
+rm -rf $htdocs/quadstor
+
+cmp=`cmp -s /quadstor/httpd/www/index.html $htdocs/index.html`
+if [ "$?" = "0" ]; then
+	rm -f $htdocs/index.html
+fi
+
 cmod=`/sbin/kldstat | grep coredev`
 if [ "$cmod" = "" ]; then
 	return
 fi
+
 
 /etc/rc.d/quadstor stop > /dev/null 2>&1
 cmod=`/sbin/kldstat | grep coredev`
