4036938e2138155b017f4da3c6faf9765c28ab7c
Shivaram U <shivaram.u@quadstor.com>
2013-07-27 05:48:12 -0400

    pgpatch ensure all pg logck files are removed

--- pgsql/scripts/pgpatch.sh
+++ pgsql/scripts/pgpatch.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 os=`uname`
 /quadstor/pgsql/etc/pgsql stop > /dev/null 2>&1
+pkill -f /quadstor/pgsql/bin/postmaster > /dev/null 2>&1
+pkill -f /quadstor/pgsql/bin/postgres > /dev/null 2>&1
+rm -f /var/run/postmaster.9988.pid > /dev/null 2>&1
+rm -f /quadstor/pgsql/data/postmaster.pid > /dev/null 2>&1
+rm -f /var/lock/subsys/pgsql > /dev/null 2>&1
+rm -f /tmp/.s.PGSQL.9988* > /dev/null 2>&1
+
 
 /quadstor/pgsql/etc/pgsql start > /dev/null 2>&1
 sleep 5
