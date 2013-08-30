d0385f96d5e9428318bd917b452cbbfb69401c77
Shivaram U <shivaram.u@quadstor.com>
2013-08-30 12:16:39 +0530

    rhel5 build fixes

--- build/rhel5.sh
+++ build/rhel5.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -x
-sh buildinit.sh
+sh buildinit.sh rhel5
 rpmbuild -bb quadstorcore.spec && rpmbuild -bb quadstorclient.spec && rpmbuild -bb quadstoritf.spec && rpmbuild -bb quadstoritfmin.spec
